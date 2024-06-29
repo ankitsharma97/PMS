af170ba97930fffd0de49f157c762b3dcd789450
Ankit sharma <ankit972125@gmail.com>
2024-06-29 22:28:12 +0530

    2st

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('DashBoard/',views.DashBoard,name='DashBoard'),
-    path('login/',views.login,name='login'),
+    path('',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
     path('addQ/',views.AddQuestion,name='addQ'),
     path('editQ/<int:Pid>',views.EditQuestion,name='editQ'),
